90b008f3f18f8fb77586219a933c26fd2b2c94cb
PeeHaa <info@pieterhordijk.com>
2015-03-07 11:48:31 +0100

    Setup bootstrap file

---
+++ bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Bootstrap the project
+ *
+ * PHP version 5.5
+ *
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    1.0.0
+ */
+namespace Examinr;
