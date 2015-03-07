ff37bde5c74ad86cc661a619a3417a4ee6f599c8
PeeHaa <info@pieterhordijk.com>
2015-03-07 21:49:27 +0100

    Added CSRF form

---
+++ src/Form/Implementation/Csrf.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * CSRF form
+ *
+ * Simple form which only contains a CSRF token field
+ *
+ * PHP version 5.5
+ *
+ * @category   Examinr
+ * @package    Form
+ * @subpackage Implementation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace Examinr\Form\Implementation;
+
+use Examinr\Form\AbstractForm;
+
+/**
+ * CSRF form
+ *
+ * @category   Examinr
+ * @package    Form
+ * @subpackage Implementation
+ * @author     Pieter Hordijk <info@pieterhordijk.com>>
+ */
+class Csrf extends AbstractForm
+{
+    /**
+     * Sets up the fields of the form
+     */
+    protected function setupFields()
+    {
+    }
+}
