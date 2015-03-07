51683279fb71297c1d8516b7ce6e59fe0a3b527e
PeeHaa <info@pieterhordijk.com>
2015-03-07 17:42:24 +0100

    Added missing texts

--- texts/en_US.php
+++ texts/en_US.php
@@ -2,4 +2,15 @@
 
 $texts = [
     'site.name'                                         => 'Examinr',
+    'site.description'                                  => 'Examinr runs several code quality tools on your code bases and informs you when there are problems.',
+    'site.github'                                       => 'Project on GitHub',
+
+    'login.form.title'                                  => 'Log in',
+    'login.form.field.email'                            => 'Email',
+    'login.form.field.password'                         => 'Password',
+    'login.form.field.rememberme'                       => 'Remember me',
+    'login.form.field.submit'                           => 'Log in',
+
+    'page.not-found.title'                              => 'Not found',
+    'page.not-found.description'                        => 'The page you tried to access could not be found.',
 ];
