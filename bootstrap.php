c65f78a7737b899fb4df3826a2fdc05e67596011
PeeHaa <info@pieterhordijk.com>
2015-03-07 11:56:43 +0100

    Load the environment

--- bootstrap.php
+++ bootstrap.php
@@ -6,7 +6,7 @@
  *
  * @author     Pieter Hordijk <info@pieterhordijk.com>
  * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
- * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @license    See the LICENSE file
  * @version    1.0.0
  */
 namespace Examinr;
@@ -15,3 +15,8 @@ namespace Examinr;
  * Setup the project autoloader
  */
 require_once __DIR__ . '/vendor/autoload.php';
+
+/**
+ * Setup the environment
+ */
+require_once __DIR__ . '/init.deployment.php';
