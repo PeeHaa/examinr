7d25a66a812b27d5a58382ca35986ae8a5bf9e91
PeeHaa <info@pieterhordijk.com>
2015-03-07 15:13:26 +0100

    Added index controller

---
+++ src/Presentation/Controller/Index.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Index controller
+ *
+ * PHP version 5.5
+ *
+ * @category   Examinr
+ * @package    Presentation
+ * @subpackage Controller
+ * @author     Pieter Hordijk <pieter@mindwarp.nl>
+ * @copyright  Copyright (c) 2015 Mindwarp Rotterdam <http://mindwarp.nl>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace Examinr\Presentation\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Examinr\Presentation\Template\Html;
+
+/**
+ * Index controller
+ *
+ * @category   Examinr
+ * @package    Presentation
+ * @subpackage Controller
+ * @author     Pieter Hordijk <pieter@mindwarp.nl>
+ */
+class Index
+{
+    /**
+     * @var \Symfony\Component\HttpFoundation\Response The HTTP response
+     */
+    private $response;
+
+    /**
+     * Creates instance
+     *
+     * @param \Symfony\Component\HttpFoundation\Response $response The HTTP response
+     */
+    public function __construct(Response $response)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * Renders the home page
+     *
+     * @param \Examinr\Presentation\Template\Html $template A HTML template renderer
+     *
+     * @return \Symfony\Component\HttpFoundation\Response The HTTP response
+     */
+    public function index(Html $template)
+    {
+        $this->response->setContent($template->renderPage('/index.phtml'));
+
+        $this->response->setStatusCode(Response::HTTP_OK);
+        $this->response->headers->set('Content-Type', 'text/html');
+
+        return $this->response;
+    }
+}
