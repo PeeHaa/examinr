f1a5012eb7dbd36a0633dfe8132759c1c6879532
PeeHaa <info@pieterhordijk.com>
2015-03-10 20:44:18 +0100

    Added not found exception

---
+++ src/Download/NotFoundException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Exception which gets thrown when trying to download from a source that doesn't exist
+ *
+ * PHP version 5.5
+ *
+ * @category   Examinr
+ * @package    Download
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace Examinr\Download;
+
+/**
+ * Exception which gets thrown when trying to download from a source that doesn't exist
+ *
+ * @category   Examinr
+ * @package    Download
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class NotFoundException extends \Exception
+{
+}
